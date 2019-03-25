6641dc5ed366e72abc54d3a160b638a9a88b1176
Dan Hwang <danhwangfkpc@gmail.com>
2019-03-25 12:07:45 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name 
+    @roster = {}
+  end 
+  
+  def add_student(name, level)
+    roster[level] ||= []
+    roster[level] << name
+  end
+  
+  def grade(level)
+    roster.each do |x, y|
+      if x == level 
+        return y 
+      end 
+    end 
+  end 
+  
+  def sort
+    new_array = {}
+    roster.each do |x, y|
+      new_array[x] = y.sort 
+    end 
+    new_array
+  end
+  
+end 
